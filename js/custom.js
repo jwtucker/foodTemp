cb15e00d8116f881a65e9b0bbd52c45c7e705d20
jwtucker <tuckerjeremyw@gmail.com>
2015-04-14 00:32:26 -0400

    Navbar Additions

--- js/custom.js
+++ js/custom.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
 		$(this).find('.galleryItemColor').stop().fadeOut();
 	});
 
+	$('.navBarScrollWrapper').hide();
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 400){
+			$('.navBarScrollWrapper').slideDown();
+		}
+		else{
+			$('.navBarScrollWrapper').slideUp();			
+		}
+	});
+
 });
